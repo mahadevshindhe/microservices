e5d844dd948001e2948c12dbde3815f7341f04a1
mahadeva.shindhe <mahadeva.shindhe@tavant.com>
2019-01-22 18:29:58 +0530

    implementation of discovery server, service and client

--- discovery-server/service/src/main/java/com/example/demo/ServiceApplication.java
+++ discovery-server/service/src/main/java/com/example/demo/ServiceApplication.java
@@ -1,13 +1,28 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@RestController
 public class ServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceApplication.class, args);
+	}
+	
+	@Autowired
+	private Environment environment;
+	
+	@GetMapping("/")
+	public String getMessage() {
+		return "Hello from service (port: " + environment.getProperty("local.server.port")+")";
 	}
 
 }
