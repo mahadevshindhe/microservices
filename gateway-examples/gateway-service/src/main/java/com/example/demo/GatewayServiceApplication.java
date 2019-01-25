db7e5bdf676cf02f5ee37bf15a97b1424c2daa38
mahadeva.shindhe <mahadeva.shindhe@tavant.com>
2019-01-25 12:35:00 +0530

    initial development

--- gateway-examples/gateway-service/src/main/java/com/example/demo/GatewayServiceApplication.java
+++ gateway-examples/gateway-service/src/main/java/com/example/demo/GatewayServiceApplication.java
@@ -2,13 +2,27 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import com.example.demo.filters.AddRequestHeaderFilter;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
 public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
 	}
+	
+	@Bean
+	public AddRequestHeaderFilter addRequestHeaderFilter() {
+		return new AddRequestHeaderFilter();
+	}
 
 }
+
+/* curl -X GET http://localhost:8080/proxypath */
 
--- gateway-examples/hello-service/src/main/java/com/example/demo/HelloServiceApplication.java
+++ gateway-examples/hello-service/src/main/java/com/example/demo/HelloServiceApplication.java
@@ -2,13 +2,26 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@RestController
 public class HelloServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloServiceApplication.class, args);
 	}
 
+	@GetMapping
+	public String getMessage(@RequestHeader("x-location") String location) {
+		return "Hello from " + location;
+	}
 }
 
+/*
+	curl -X GET http://localhost:9090/ -H 'x-location: India'
+*/
